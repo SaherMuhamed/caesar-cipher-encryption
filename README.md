8d18afb6383c9586d1fa53136efb334c0fb544f7
Saher Mohamed <sahermuhamed21@gmail.com>
2023-07-17 01:40:49 +0300

    Update project

---
+++ README.md
@@ -0,0 +1,40 @@
+# Caesar Cipher
+This is a Python script that implements the Caesar Cipher encryption and decryption algorithm. The Caesar Cipher is a substitution cipher where each letter in the plaintext is shifted a certain number of positions down or up the alphabet.
+
+## Usage
+1. Run the script in a Python environment.
+2. Follow the prompts to choose the encryption or decryption mode, enter the text to process, and specify the shift number.
+3. The script will display the resulting text according to the chosen mode.
+
+## Prerequisites
+- Python 3.x
+
+## Screenshots
+![](screenshots/Screenshot_2023-07-17_013305.png)
+
+## Installation
+1. Clone the repository or download the script directly.
+2. Ensure you have Python 3.x installed on your machine.
+
+## Features
+- It supports both `encrypt` and `decrypt` process.
+- All you have to do is just provide the `shift key` to encrypt or decrypt.
+## How to Run
+1. Open a terminal or command prompt.
+2. Navigate to the directory where the script is located.
+3. Run the following command:
+    ```commandline
+    python main.py
+    ```
+
+4. Follow the on-screen instructions to interact with the script. Example:
+    ```text
+    Type 'encode' to encrypt, type 'decode' to decrypt: encode
+    Type your message: hello
+    Type the shift number: 3
+    The encode text is khoor
+    
+    Type 'yes' if you want to go again. Otherwise type 'no'.
+    no
+    Goodbye..
+    ```
--- main.py
+++ main.py
@@ -1,51 +1,58 @@
+import sys
 import art
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print(art.logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-shift %= 26
 
 def caesar(input_text, input_shift, input_direction):
-  end_text = ""
-  for letter in input_text:
-    if input_direction == "encode":
-      if letter in alphabet:
-        position = alphabet.index(letter)
-        new_position = position + input_shift
-        new_letter = alphabet[new_position]
-        end_text += new_letter
-      else:
-        end_text += letter  
-    elif input_direction == "decode":
-      if letter in alphabet:
-        position = alphabet.index(letter)
-        new_position = position - input_shift
-        new_letter = alphabet[new_position]
-        end_text += new_letter
-      else:
-        end_text += letter  
-    else:
-      print("You entered wrong command") 
-  print(f"The {input_direction} text is {end_text}\n")
-       
+    end_text = ""
+    for letter in input_text:
+        if input_direction == "encode":
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position + input_shift
+                new_letter = alphabet[new_position]
+                end_text += new_letter
+            else:
+                end_text += letter
+        elif input_direction == "decode":
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position - input_shift
+                new_letter = alphabet[new_position]
+                end_text += new_letter
+            else:
+                end_text += letter
+        else:
+            print("You entered wrong command")
+    print(f"The {input_direction} text is {end_text}\n")
 
-caesar(text, shift, direction)
 
 while True:
-  ask_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
-  if ask_again == "yes":
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    try:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+        text = input("Type your message: ").lower()
+        shift = int(input("Type the shift number: "))
 
-    shift %= 26
+        shift %= 26
+        caesar(text, shift, direction)
 
-    caesar(text, shift, direction)
-  elif ask_again == "no":
-    print("Goodbye..")
-    break  
+        ask_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+        if ask_again == "yes":
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+            text = input("Type your message:\n").lower()
+            shift = int(input("Type the shift number:\n"))
+
+            shift %= 26
+
+            caesar(text, shift, direction)
+        elif ask_again == "no":
+            print("Goodbye..")
+            break
+    except KeyboardInterrupt:
+        print("\n[-] ctrl + c detected, program terminated")
+        sys.exit(0)
